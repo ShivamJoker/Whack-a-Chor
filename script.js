59d52d1d41998049b765395736fc8a13cb24910f
cssdude <email@cssdude.com>
2021-05-23 20:34:17 +0530

    Game

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 (() =>{
-    let ulElem = document.querySelector("ul");
-    let liElem = '';
-    const fClasses = ['one', 'two', 'three', 'four', 'five'];
-    const fPersons = ['Intern', 'SDE', 'Problem Solver', 'Innocent', 'Coder'];
-
-    for(let i=0; i<1500; i++){
-        liElem += '<li><span class="bubble"></span></li>';
-    };
-    ulElem.innerHTML = liElem;
+    const hammer = document.querySelector(".hammer");
+    const elemArray = document.querySelectorAll("ul li");
+    const bang = document.getElementById("bang");
+    const fPersons = ['Hey! I am <br />Intern Srivastava<br /><span>I know Tuti Futi English</span>', 'Hey! I am  <br />SDE Garg<br /><span>I like stupid Comments</span>', 'Hey! I am  <br />Innocent Singh<br /><span>I steal code from GitHub</span>', 'Hey! I am  <br />Problem Mishra<br /><span>I support Innocent problems</span>'];
 
     setInterval(() =>{
-        let liElements = ulElem.querySelectorAll("li");
-        let randomNumberBig = Math.floor(Math.random() * 800);
-        let randomNumberSmall = Math.floor(Math.random() * 5);
-        for(let i=0; i<liElements.length; i++){
-            liElements[i].querySelector('.bubble').style.display = 'none';
-            liElements[i].querySelector('.bubble').classList.remove(liElements[i].classList.item(1));
+        for(let i=0; i<elemArray.length; i++){
+            elemArray[i].querySelector('.bunny').classList.remove(elemArray[i].querySelector('.bunny').classList.item(1));
+                elemArray[i].addEventListener('click', (e)=>{
+                    if(e.target.querySelector('.bunny').classList.contains('show')){
+                        e.target.querySelector('.bunny').classList.remove('show');
+                        bang.play();
+                    }
+                });
         };
-        liElements[randomNumberBig].querySelector('.bubble').classList.add(fClasses[randomNumberSmall]);
-        liElements[randomNumberBig].querySelector('.bubble').innerHTML = fPersons[randomNumberSmall];
-        liElements[randomNumberBig].querySelector('.bubble').style.display = 'block';
-    }, 2000);
+
+        let randomNumber = Math.floor(Math.random() * 4);
+        elemArray[randomNumber].querySelector('.bunny i').innerHTML = fPersons[randomNumber];
+        elemArray[randomNumber].querySelector('.bunny').classList.add('show');
+    }, 4000);
+
+    document.addEventListener('mousemove', (evt)=>{
+        hammer.style.transform = `translate(${evt.screenX}px, ${evt.screenY}px)`;
+    });
+
+    document.addEventListener('mousedown', (evt)=>{
+        hammer.style.transform = `translate(${evt.screenX}px, ${evt.screenY}px) rotate(-45deg)`;
+    });
+
+    document.addEventListener('mouseup', (evt)=>{
+        setTimeout(()=>{
+            hammer.style.transform = `translate(${evt.screenX}px, ${evt.screenY}px) rotate(0deg)`;
+        }, 200);
+    });
 })();
